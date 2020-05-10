c4bf704b4b2778e8eb430f7567e8224b0818955b
David. J <codewithjalloh@gmail.com>
2020-05-10 17:36:32 +0100

    phone number card widget added

--- lib/main.dart
+++ lib/main.dart
@@ -25,8 +25,43 @@ class MyApp extends StatelessWidget {
                   color: Colors.white,
                   fontWeight: FontWeight.bold,
                   fontFamily: 'GFS Neohellenic',
-
                 ),),
+//                SizedBox(
+//                  width:20.0,
+//                  height: 150.0,
+//                child: Divider(
+//                  color: Colors.teal.shade900,
+//                ),),
+                Text('Philosopher',
+                  style:TextStyle(
+                    fontFamily: 'GFS Neohellenic',
+                    color: Colors.teal.shade100,
+                    fontSize: 20.0,
+                    letterSpacing: 2.5,
+                  ) ,
+                ),
+                Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10.0,
+                    horizontal: 25.0,
+                  ),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.black,
+                    ),
+                    title: Text(
+                      '111-669-669-1',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20.0,
+                        fontFamily:'GFS Neohellenic',
+                      ),
+                    ),
+                  ),
+                ),
+
 
               ],
 
